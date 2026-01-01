161cfc6085e6655262aa4e94d17a17e2046e3bfb
superkookai <weerawut@live.com>
2026-01-01 13:41:51 +0700

    Initial Commit

---
+++ MultiMapA/MultiMapAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapAApp.swift
+//  MultiMapA
+//
+//  Created by Weerawut on 1/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
